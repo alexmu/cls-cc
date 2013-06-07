06dff911e570eccd1435a21efb21ea17b15bb1dd
alexmu <mwm8412@163.com>
2013-06-07 10:17:53 +0800

    init

--- src/cn/ac/iie/cls/cc/slavecc/SlaveHandler.java
+++ src/cn/ac/iie/cls/cc/slavecc/SlaveHandler.java
@@ -8,5 +8,5 @@ import java.util.Map;
 
 public interface SlaveHandler {
 
-    public String execute(String pRequestContent) throws Exception;
+    public String execute(String pRequestContent) throws Exception;    
 }
--- src/cn/ac/iie/cls/cc/slavecc/dataetl/DataETLExecuteHandler.java
+++ src/cn/ac/iie/cls/cc/slavecc/dataetl/DataETLExecuteHandler.java
@@ -10,13 +10,21 @@ import cn.ac.iie.cls.cc.slavecc.SlaveHandler;
  *
  * @author alexmu
  */
-public class DataETLExecuteHandler implements SlaveHandler {
+public class DataETLExecuteHandler implements SlaveHandler,Runnable {
 
+    private static DataETLExecuteHandler dataETLExecuteHandler = null;
+    
+    @Override
     public String execute(String pRequestContent) throws Exception {
         String result = null;
 
         result = pRequestContent != null && !pRequestContent.isEmpty() ? pRequestContent : this.toString();
         return result;
+    }
+    
+    @Override
+    public void run(){
+        
     }
 
     public static void main(String[] args) {
