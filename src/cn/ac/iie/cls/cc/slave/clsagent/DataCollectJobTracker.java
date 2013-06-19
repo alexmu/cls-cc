f21b071bb79f3b27829c0253b4450032cd86ebd3
alexmu <mwm8412@163.com>
2013-06-19 09:39:15 +0800

    init

--- src/cn/ac/iie/cls/cc/slave/clsagent/DataCollectJobTracker.java
+++ src/cn/ac/iie/cls/cc/slave/clsagent/DataCollectJobTracker.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
@@ -22,11 +24,12 @@ import org.apache.http.impl.client.DefaultHttpClient;
  */
 public class DataCollectJobTracker implements Runnable {
 
-    private static BlockingQueue<DataCollectJob> dataCollectJobWaitingList = new LinkedBlockingQueue<DataCollectJob>();
-    private static Map<String, DataCollectJob> executingDataCollectJobSet = new HashMap<String, DataCollectJob>();
+    private BlockingQueue<DataCollectJob> dataCollectJobWaitingList = new LinkedBlockingQueue<DataCollectJob>();
+    private Map<String, DataCollectJob> executingDataCollectJobSet = new HashMap<String, DataCollectJob>();
+    private Lock executingDataCollectJobSetLock = new ReentrantLock();
     private static DataCollectJobTracker dataCollectJobTracker = null;
 
-    private DataCollectJobTracker() {        
+    private DataCollectJobTracker() {
     }
 
     public static synchronized DataCollectJobTracker getDataCollectJobTracker() {
@@ -46,23 +49,25 @@ public class DataCollectJobTracker implements Runnable {
     }
 
     public void removeJob(DataCollectJob pDataCollectJob) {
-        synchronized (executingDataCollectJobSet) {
-            executingDataCollectJobSet.remove(pDataCollectJob.getProcessJobInstanceID());
-        }
+        executingDataCollectJobSetLock.lock();
+        executingDataCollectJobSet.remove(pDataCollectJob.getProcessJobInstanceID());
+        executingDataCollectJobSetLock.unlock();
     }
 
     public void appendTask(String pDataProcessInstanceId, List<DataCollectTask> pDataCollectTaskList) {
-        synchronized (executingDataCollectJobSet) {
-            DataCollectJob dataCollectJob = executingDataCollectJobSet.get(pDataProcessInstanceId);
-            dataCollectJob.appendTask(pDataCollectTaskList);
-        }
+        executingDataCollectJobSetLock.lock();
+        DataCollectJob dataCollectJob = executingDataCollectJobSet.get(pDataProcessInstanceId);
+        executingDataCollectJobSetLock.unlock();
+        dataCollectJob.appendTask(pDataCollectTaskList);
+
     }
 
     public void responseTask(String pDataProcessInstanceId, List<DataCollectTask> pDataCollectTaskList) {
-        synchronized (executingDataCollectJobSet) {
-            DataCollectJob dataCollectJob = executingDataCollectJobSet.get(pDataProcessInstanceId);
-            dataCollectJob.responseTask(pDataCollectTaskList);
-        }
+        executingDataCollectJobSetLock.lock();
+        DataCollectJob dataCollectJob = executingDataCollectJobSet.get(pDataProcessInstanceId);
+        executingDataCollectJobSetLock.unlock();
+        dataCollectJob.responseTask(pDataCollectTaskList);
+
     }
 
     @Override
@@ -85,43 +90,24 @@ public class DataCollectJobTracker implements Runnable {
                     reqEntity.setContentType("binary/octet-stream");
                     reqEntity.setChunked(true);
                     httppost.setEntity(reqEntity);
+                    executingDataCollectJobSetLock.lock();
                     HttpResponse response = httpClient.execute(httppost);
                     System.out.println(response.getStatusLine());
                     httppost.releaseConnection();
                     succeeded = true;
                 } catch (Exception ex) {
                     ex.printStackTrace();
-                }
-                //end
-                if (succeeded) {
-                    executingDataCollectJobSet.put(dataCollectJob.getProcessJobInstanceID(), dataCollectJob);
-                } else {
-                    dataCollectJobWaitingList.put(dataCollectJob);
+                } finally {
+                    //end
+                    if (succeeded) {
+                        executingDataCollectJobSet.put(dataCollectJob.getProcessJobInstanceID(), dataCollectJob);
+                        executingDataCollectJobSetLock.unlock();
+                    } else {
+                        dataCollectJobWaitingList.put(dataCollectJob);
+                    }
                 }
             } catch (Exception ex) {
             }
         }
-    }
-
-    public static void main(String[] args) {
-        String dataProcessDesc = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
-                + "<requestParams>"
-                + "<processJobInstanceId>8a8081e8288b759201288b97c73a000e</processJobInstanceId>"
-                + "<processConfig>"
-                + "<operator name=\"operator_id_123\" class=\"GetherDataFromLocalFS\" version=\"1.0\" x=\"-1\" y=\"-1\">"
-                + "<parameter name=\"name\">917mt6</parameter>"
-                + "<parameter name=\"srcpath\">/home/iie</parameter>"
-                + "<parameter name=\"hdfsPath\">hdfs://192.168.111.128:9000/test/123/456</parameter>"
-                + "<parameter name=\"timeout\">10000000</parameter>"
-                + "<parameterlist name=\"splitConfig\">"
-                + "<parametermap name=\"exe\" value=\"true\"/>"
-                + "<parametermap name=\"linesplitor\" value=\"\n\"/>"
-                + "<parametermap name=\"num\" value=\"1000\"/>"
-                + "</parameterlist>"
-                + "</operator>"
-                + "</processConfig>"
-                + "</requestParams>";
-        DataCollectJobTracker.getDataCollectJobTracker().appendJob(new DataCollectJob("8a8081e8288b759201288b97c73a000e", dataProcessDesc));
-        new DataCollectJobTracker().run();
     }
 }
